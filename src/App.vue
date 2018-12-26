8c2bb0d96ac8bf3cda75c1511d85f563f7468c3f
Sinan <sinan@sinan.sinan>
2018-12-26 19:20:39 +0100

    spajanje

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <input v-model="ime" placeholder="Ime"><br>
+    <input v-model="prezime" placeholder="Prezime">
+    <p>Ime i prezime: {{ ime }} {{ prezime }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default {
+  name: 'Moja aplikacija',
+  components: {
+    
+  },
+  data(){
+    return {
+      ime:'',
+      prezime:'',
+    };
+  },
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
